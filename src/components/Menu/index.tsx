c9f548ef3c68ce1b71ba99652baf89677550d11f
Afonso Alcantara <alcantara.afonso@gmail.com>
2026-09-05 23:19:41 -0300

    MainFor and page's folder

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,75 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+
+type AvailabeThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailabeThemes>(() => {
+    return (localStorage.getItem("theme") as AvailabeThemes) || "dark";
+  });
+
+  function handleThemeClick(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); //Não executa o link
+    setTheme((prevState) => {
+      const nextTheme = prevState === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]); //Diz que este useEffect executa quando theme muda
+
+  //Lookup Table
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para home"
+        title="Ir para home"
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ver histórico"
+        title="Ver histórico"
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurações"
+        title="Configurações"
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href="#">
-        <SunIcon />
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar tema"
+        title="Mudar tema"
+        onClick={handleThemeClick}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function Home() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+          eligendi fugit voluptatem, facilis vitae sint quibusdam aliquam
+          aperiam tempore voluptas error ipsa architecto dicta voluptates?
+          Numquam sed nam reprehenderit cupiditate!
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
